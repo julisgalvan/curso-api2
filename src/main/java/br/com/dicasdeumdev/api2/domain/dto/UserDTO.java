957af828386723883df99b2e3e83f92dba70b0f7
julisgalvan <julisgalvan@gmail.com>
2024-06-06 16:38:50 -0300

    endpoint create para user

--- src/main/java/br/com/dicasdeumdev/api2/domain/dto/UserDTO.java
+++ src/main/java/br/com/dicasdeumdev/api2/domain/dto/UserDTO.java
@@ -1,6 +1,6 @@
 package br.com.dicasdeumdev.api2.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +16,7 @@ public class UserDTO {
         private Integer id;
         private String name;
         private String email;
-        @JsonIgnore
+        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
         private String password;
 
 }
--- src/main/java/br/com/dicasdeumdev/api2/services/UserService.java
+++ src/main/java/br/com/dicasdeumdev/api2/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.dicasdeumdev.api2.services;
 
 import br.com.dicasdeumdev.api2.domain.User;
+import br.com.dicasdeumdev.api2.domain.dto.UserDTO;
 
 import java.util.List;
 
@@ -8,4 +9,5 @@ public interface UserService {
 
     User findById (Integer id);
     List<User> findAll();
+    User create(UserDTO obj);
 }
