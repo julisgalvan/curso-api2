32cfe5fd3d71dabcd46d53a659ddbeb6962be400
julisgalvan <julisgalvan@gmail.com>
2024-06-10 16:37:34 -0300

    excecao para email ja cadastrado

--- src/main/java/br/com/dicasdeumdev/api2/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/dicasdeumdev/api2/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.dicasdeumdev.api2.resources.exceptions;
 
+import br.com.dicasdeumdev.api2.services.exceptions.DataIntegratyViolationException;
 import br.com.dicasdeumdev.api2.services.exceptions.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -17,5 +18,12 @@ public class ResourceExceptionHandler {
         StandardError error =
                 new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegratyViolationException.class)
+    public ResponseEntity<StandardError>DataIntegratyViolationException(DataIntegratyViolationException ex, HttpServletRequest request) {
+        StandardError error =
+                new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
