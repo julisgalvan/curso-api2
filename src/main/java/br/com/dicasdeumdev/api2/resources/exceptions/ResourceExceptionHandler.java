a263a6b6a3f7663a13a7bc552dbb8b3af0b42349
julisgalvan <julisgalvan@gmail.com>
2024-06-17 17:19:21 -0300

    removendo anotacoes nao utilizadas

--- src/main/java/br/com/dicasdeumdev/api2/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/dicasdeumdev/api2/resources/exceptions/ResourceExceptionHandler.java
@@ -1,8 +1,8 @@
 package br.com.dicasdeumdev.api2.resources.exceptions;
 
-import br.com.dicasdeumdev.api2.services.exceptions.DataIntegratyViolationException;
 import br.com.dicasdeumdev.api2.services.exceptions.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,8 +20,8 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
-    @ExceptionHandler(DataIntegratyViolationException.class)
-    public ResponseEntity<StandardError>dataIntegratyViolationException(DataIntegratyViolationException ex, HttpServletRequest request) {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError>dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
         StandardError error =
                 new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
--- src/main/java/br/com/dicasdeumdev/api2/resources/exceptions/StandardError.java
+++ src/main/java/br/com/dicasdeumdev/api2/resources/exceptions/StandardError.java
@@ -2,11 +2,9 @@ package br.com.dicasdeumdev.api2.resources.exceptions;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 @Getter
-@Setter
 @AllArgsConstructor
 public class StandardError {
     private LocalDateTime timestamp;
